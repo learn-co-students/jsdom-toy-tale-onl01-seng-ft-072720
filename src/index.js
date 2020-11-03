94aa068660d969f3ec178eb814f32dcce1b10d4f
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-11-03 16:17:38 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -12,6 +12,10 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  const form = document.querySelector("body > div.container > form");
+  form.addEventListener("submit", submitNewToy);
+
 });
 
 function getToyCollection () {
@@ -22,34 +26,57 @@ function getToyCollection () {
 
 function displayToys(dataset) {
   dataset.forEach(data => {
-  // data = data[0];
-  const toyCollection = document.querySelector("#toy-collection");
-  const div = document.createElement("div");
-  div.className = "card";
-  toyCollection.appendChild(div);
+    const toyCollection = document.querySelector("#toy-collection");
+    const div = document.createElement("div");
+    div.className = "card";
+    toyCollection.appendChild(div);
 
-  const toyName = document.createElement("h2");
-  toyName.innerText = data["name"];
-  div.appendChild(toyName);
+    const toyName = document.createElement("h2");
+    toyName.innerText = data["name"];
+    div.appendChild(toyName);
 
-  const img = document.createElement("img");
-  img.src = data["image"]
-  img.className = "toy-avatar"
-  div.appendChild(img);
+    const img = document.createElement("img");
+    img.src = data["image"]
+    img.className = "toy-avatar"
+    div.appendChild(img);
 
-  const p = document.createElement("p");
-  p.innerText = `${data.likes} likes`;
-  div.appendChild(p);
+    const p = document.createElement("p");
+    p.innerText = `${data.likes} likes`;
+    div.appendChild(p);
 
-  const button = document.createElement("button");
-  button.className = "like-btn";
-  button.innerText = "Like <3";
-  div.appendChild(button);
+    const button = document.createElement("button");
+    button.className = "like-btn";
+    button.innerText = "Like <3";
+    div.appendChild(button);
   })
+}
 
+function submitNewToy(event) {
+  event.preventDefault();
+  const name = document.querySelector("body > div.container > form > input:nth-child(2)").value;
+  const image = document.querySelector("body > div.container > form > input:nth-child(4)").value;
+  createNewToyFromForm(name, image);
+}
 
-
+function createNewToyFromForm(name, image) {
   // debugger;
+  let formData = {
+    name,
+    image,
+    likes: "0"
+  };
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(formData)
+
+  }
+  debugger;
+  fetch("http://localhost:3000/toys", configObj)
 }
 
 getToyCollection();
