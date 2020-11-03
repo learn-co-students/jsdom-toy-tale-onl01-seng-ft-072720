e1440581108cea7806dd1c928d7cf97d24962487
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-11-03 15:58:01 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,45 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function getToyCollection () {
+  fetch('http://localhost:3000/toys')
+    .then(response => response.json())
+    .then(data => displayToys(data))
+}
+
+function displayToys(dataset) {
+  dataset.forEach(data => {
+  // data = data[0];
+  const toyCollection = document.querySelector("#toy-collection");
+  const div = document.createElement("div");
+  div.className = "card";
+  toyCollection.appendChild(div);
+
+  const toyName = document.createElement("h2");
+  toyName.innerText = data["name"];
+  div.appendChild(toyName);
+
+  const img = document.createElement("img");
+  img.src = data["image"]
+  img.className = "toy-avatar"
+  div.appendChild(img);
+
+  const p = document.createElement("p");
+  p.innerText = `${data.likes} likes`;
+  div.appendChild(p);
+
+  const button = document.createElement("button");
+  button.className = "like-btn";
+  button.innerText = "Like <3";
+  div.appendChild(button);
+  })
+
+
+
+  // debugger;
+}
+
+getToyCollection();
+
+const toyCollection = document.querySelector("#toy-collection")
